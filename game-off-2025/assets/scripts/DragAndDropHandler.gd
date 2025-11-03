9645bf7e4477a184f1e41b215f74cbb9d906ff9d
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2025-11-04 00:43:30 +0100

    Add drag and drop

---
+++ game-off-2025/assets/scripts/DragAndDropHandler.gd
@@ -0,0 +1,83 @@
+extends Node2D 
+
+const DUPLICATE_STRUCTURE_ONLY = 0  # no signals, no scripts, no groups
+
+var is_dragging = false;
+var dragged_control : Control; ## the control currently being dragged
+var control_copy : Control; ## the control we create to visually move around while dragging
+var dragged_control_offset : Vector2; ## the position offset to hold the control from where the mouse was
+var drop_receivers : Array[DropReceiver]; ## list of control able to receive the drop
+
+func _ready() -> void:
+	var nodes : Array;
+	find_drop_receivers(get_tree().get_root(), nodes);
+	for node in nodes :
+		drop_receivers.append(node);
+
+func find_drop_receivers(node: Node, result : Array) -> void:
+	if node is DropReceiver :
+		result.push_back(node);
+	for child in node.get_children():
+		if child.name == "Tilemap area":
+			print("prout");
+		find_drop_receivers(child, result);
+
+func _process(_delta: float) -> void:
+	if is_dragging:
+		control_copy.position = get_local_mouse_position() + dragged_control_offset;
+
+func on_drag_input():
+	dragged_control = get_control_to_drag();
+	if dragged_control == null: return;
+	drag();
+
+func on_drop_input():
+	if !is_dragging: return;
+	
+	drop();
+
+func drag():
+	create_movable_copy(dragged_control);
+	dragged_control.visible = false;
+	is_dragging = true;
+
+func drop():
+	on_drop();
+	
+	is_dragging = false;
+
+func on_drop():
+	var drop_receiver = get_control_to_drop_in();
+	if drop_receiver == null:
+		cancel_drag();
+	drop_receiver.on_drop(dragged_control);
+
+func cancel_drag() :
+	destroy_movable_copy();
+	dragged_control.visible = true;
+	is_dragging = false;
+
+func get_control_to_drag() -> Control:
+	var control_child : Control = get_viewport().gui_get_hovered_control();
+	if control_child == null: return;
+	
+	## we get the root node of the instantiated scene 
+	var control = control_child.owner;
+	if !control.has_meta("Draggable") || !control.get_meta("Draggable"): return;
+		
+	return control;
+
+func get_control_to_drop_in() -> DropReceiver:
+	for drop_receiver in drop_receivers:
+		if drop_receiver.get_rect().has_point(get_local_mouse_position()) :
+			return drop_receiver;
+	return null;
+
+func create_movable_copy(control_to_copy: Control):
+	control_copy = control_to_copy.duplicate(DUPLICATE_STRUCTURE_ONLY);
+	dragged_control_offset = control_to_copy.global_position - get_local_mouse_position();
+	control_copy.position = get_local_mouse_position() + dragged_control_offset;
+	get_tree().current_scene.add_child(control_copy);
+
+func destroy_movable_copy(): 
+	control_copy.queue_free();
--- game-off-2025/assets/scripts/Tilecard.gd
+++ game-off-2025/assets/scripts/Tilecard.gd
@@ -1,4 +1,4 @@
-extends TextureRect
+extends Control
 class_name TileCard
 
 const tile_card_scene: PackedScene = preload("res://scenes/TileCard.tscn");
@@ -6,6 +6,7 @@ const tile_card_scene: PackedScene = preload("res://scenes/TileCard.tscn");
 @export var card_name : RichTextLabel;
 @export var card_description : RichTextLabel;
 @export var card_sprite: TextureRect;
+var card_tile_sprite_atlas_coordinates : Vector2i;
 
 ## tmp
 var card_names = ["river", "hill", "grove", "plain"];
@@ -21,5 +22,6 @@ func setup() :
 	card_name.text = card_names[randi() % card_names.size()];
 	card_description.text = card_descriptions[randi() % card_descriptions.size()];
 	var card_sprite_texture : AtlasTexture = card_sprite.texture;
-	var atlas_coords = card_sprite_atlas_coords[randi() % card_sprite_atlas_coords.size()] * 16;
+	card_tile_sprite_atlas_coordinates = card_sprite_atlas_coords[randi() % card_sprite_atlas_coords.size()];
+	var atlas_coords = card_tile_sprite_atlas_coordinates * 16;
 	card_sprite_texture.region = Rect2(atlas_coords.x, atlas_coords.y , 16, 16);
