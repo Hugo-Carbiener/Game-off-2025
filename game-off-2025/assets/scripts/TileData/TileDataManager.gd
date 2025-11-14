f0d6bcdbbc04f78b01bc803ba878de6c251d4c74
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2025-11-14 23:49:35 +0100

    Add sprites

--- game-off-2025/assets/scripts/TileData/TileDataManager.gd
+++ game-off-2025/assets/scripts/TileData/TileDataManager.gd
@@ -5,18 +5,20 @@ const TILE_CUSTOM_DATA_DAMAGE_KEY = "damage";
 const TILE_CUSTOM_DATA_DESCRIPTION_KEY = "description";
 const TILE_CUSTOM_DATA_PLAYABLE_KEY = "is_playable";
 
-const tile_set: TileSet = preload("res://assets/tiles/tiles.tres");
+const tile_set: TileSet = preload("res://assets/tiles/tiles_8px.tres");
 
 var tiles : Array[String] = [];
 var playable_tiles : Array[String] = [];
 var tile_damages = {"none" = 0, "low" = 1, "medium" = 2, "high" = 3};
 var tile_dictionnary : Dictionary[String, CustomTileData];
+var tile_size : Vector2i;
 
 func _ready() -> void:
 	load_tile_data();
 
 func load_tile_data():
 	var source : TileSetAtlasSource = tile_set.get_source(0);
+	tile_size = source.texture_region_size;
 	for tile_number in source.get_tiles_count():
 		var atlas_coordinates = source.get_tile_id(tile_number);
 		var tile_data = source.get_tile_data(atlas_coordinates, 0);
@@ -24,7 +26,7 @@ func load_tile_data():
 		var tile_damage = tile_damages.get(tile_data.get_custom_data(TILE_CUSTOM_DATA_DAMAGE_KEY));
 		var tile_description = tile_data.get_custom_data(TILE_CUSTOM_DATA_DESCRIPTION_KEY);
 		var is_playable = tile_data.get_custom_data(TILE_CUSTOM_DATA_PLAYABLE_KEY);
-		var atlas_texture_coordinates = atlas_coordinates * source.texture_region_size;
+		var atlas_texture_coordinates = atlas_coordinates * tile_size;
 		var custom_tile_data = CustomTileData.create(tile_name, 
 			tile_damage, 
 			tile_description, 
