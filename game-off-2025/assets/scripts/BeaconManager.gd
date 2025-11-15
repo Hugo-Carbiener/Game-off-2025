fe61c946db00b74353a6a2d2a7e0f7615f668760
Guillaume <guillaume@1kb.software>
2025-11-15 23:00:28 +0100

    Add basic HUD with beacon HP
    Closes #16

--- game-off-2025/assets/scripts/BeaconManager.gd
+++ game-off-2025/assets/scripts/BeaconManager.gd
@@ -2,18 +2,24 @@ extends Node
 class_name BeaconManager
 
 static var health: int = Constants.beacon_hp;
-
 static var instance : BeaconManager;
 
+signal beacon_hp_updated;
+
 func _ready() -> void:
+	beacon_hp_updated.emit(health)
+	
 	if instance == null:
 		instance = self;
 		
 func damage(damages: int):
 	health -= damages;
-	print_debug('remaining', health)
+	if health < 0: 
+		health = 0;
+		
+	beacon_hp_updated.emit(health)
+	
 	if health <= 0:
-		print_debug('dead')
 		get_tree().paused = true
 		get_node("../Death Screen").visible = true
 	
---
+++ game-off-2025/assets/scripts/Hud.gd
@@ -0,0 +1,16 @@
+extends Control
+
+signal updateBeaconHp;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_beacon_manager_beacon_hp_updated(health: int) -> void:
+	get_node("Beacon HP").text  = str(health)
